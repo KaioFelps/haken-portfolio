b97bdef0d4a77af96d3b025869952a682cae199f
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-10-04 01:08:57 -0300

    style: make a responsive update for Footer ui component

--- src/ui/footer.tsx
+++ src/ui/footer.tsx
@@ -1,11 +1,16 @@
 import { IconBrandGithubFilled, IconBrandInstagram, IconBrandLinkedin } from "@tabler/icons-react";
+import clsx from "clsx";
 
 import { config } from "@/app/layout";
 
 export function Footer() {
   return (
-    <footer className="flex items-center justify-between px-6 py-3 mt-28">
-      <span className="font-light text-sm">© Todos os direitos reservados.</span>
+    <footer className="
+        flex items-center justify-between px-6 py-3 mt-28
+        max-xs:flex-col max-xs:justify-center max-xs:gap-3 max-xs:pb-12
+        "
+    >
+      <Copyright className="max-xs:hidden" />
       <div className="flex gap-2.5">
         <a
           href={config.socials.github}
@@ -35,6 +40,13 @@ export function Footer() {
           Instagram
         </a>
       </div>
+
+      <Copyright className="xs:hidden text-white/60" />
     </footer>
   );
 }
+
+export const Copyright = ({ className }: { className?: string }) =>
+  <span className={clsx("font-light text-sm", className && className)}>
+    © Todos os direitos reservados.
+  </span>;
