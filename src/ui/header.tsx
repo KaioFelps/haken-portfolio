e0285696309955b83d9c10e7c2cbf12e06fd98bc
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-10-04 00:47:10 -0300

    feat: header Made it stick to the top of screen and change style on scroll down

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -3,10 +3,12 @@
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import Logo from "@/assets/Logo.svg";
 
 export function Header() {
+  const [hasScrolledDown, setHasScrolledDown] = useState(false);
   const pathname = usePathname();
 
   // [path, label][]
@@ -16,8 +18,26 @@ export function Header() {
     ["/projetos", "Projetos"],
   ];
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolledDown(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="p-6 flex items-center justify-between mb-28">
+    <header className={clsx(
+      "p-6 flex items-center justify-between mb-28 sticky top-0 transition-all duration-200",
+      "border-b border-transparent",
+      !hasScrolledDown && "bg-transparent",
+      hasScrolledDown && "bg-white/[1%] backdrop-blur-lg border-white/5",
+    )}
+    >
       <img
         src={Logo.src}
         alt="Kaio Felipe"
@@ -27,7 +47,7 @@ export function Header() {
         "px-5 py-2.5 rounded-2xl bg-white/10 flex items-center gap-5",
         "prose-a:text-white/60 hover:prose-a:text-white/70 active:prose-a:text-white/80",
         "data-[active=true]:prose-a:text-white prose-a:transition-all prose-a:duration-100",
-        "prose-a:text-sm prose-a:cursor-default prose-a:font-medium"
+        "prose-a:text-sm prose-a:cursor-default prose-a:font-medium",
       )}
       >
         {routes.map(([path, label]) => (
